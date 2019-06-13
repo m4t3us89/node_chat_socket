4cf55b38656c75821cf3b80f387cc05a332416a7
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-06-13 11:23:58 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -7,17 +7,30 @@ const app = express()
 
 
 
-const server = require('http').createServer(app)
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views' , path.join(__dirname, 'public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine' , 'html')
-app.use(morgan())
+app.use(morgan('dev'))
 
 app.use('/' , (req,res)=>{
     res.render('index')
+})
+
+let messages = []
+
+io.on('connection', socket=>{
+    console.log(`Socket Conectado ${socket.id}`)
+
+    socket.emit('previousMessages', messages)
+
+    socket.on('sendMessage' , data=>{
+        messages.push(data)
+        socket.broadcast.emit('receivedMessage', data)
+    })
 })
 
 server.listen(process.env.PORT || 3000)
